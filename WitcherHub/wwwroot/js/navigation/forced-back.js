3283d7efb5ba403096070cee6c7f312d60c93f4d
Basel <bsel7897@gmail.com>
2026-03-06 14:31:23 +0300

    fixes

---
+++ WitcherHub/wwwroot/js/navigation/forced-back.js
@@ -0,0 +1,38 @@
+﻿(function () {
+    const globalName = "forcedBackNavigation";
+
+    if (window[globalName]) return;
+
+    window[globalName] = {
+        bind: function (backUrl, stateKey) {
+            if (!backUrl) return;
+
+            const key = stateKey || "__forcedBackRedirect";
+            const listenerKey = "__forcedBackListener_" + key;
+
+            function arm() {
+                const currentState = history.state || {};
+
+                if (currentState[key] === true) return;
+
+                const nextState = Object.assign({}, currentState, { [key]: true });
+                history.pushState(nextState, "", location.href);
+            }
+
+            arm();
+
+            window.addEventListener("pageshow", arm);
+
+            if (window[listenerKey]) {
+                window.removeEventListener("popstate", window[listenerKey]);
+            }
+
+            const onPopState = function () {
+                window.location.replace(backUrl);
+            };
+
+            window[listenerKey] = onPopState;
+            window.addEventListener("popstate", onPopState);
+        }
+    };
+})();
